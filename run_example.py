a8c28a4bd1b18e22b847ed20f417f34f0ebe2a2d
Jaewon Lee <jwlee2843@gmail.com>
2023-09-13 16:27:23 +0900

    [feat] add example

---
+++ run_example.py
@@ -0,0 +1,35 @@
+from rdlvy.robot_delivery import *
+from layout import layout_2way, layout_2way_simple
+from tqdm import tqdm
+
+#env = RobotDelivery(n_agents=4,
+#                msg_bits=0,
+#                sensor_range=3,
+#                request_queue_size=5,
+#                max_inactivity_steps=10000,
+#                max_steps=10000,
+#                reward_type=RewardType.TWO_STAGE,
+#                layout=layout_2way_simple,
+#                block_size="big",
+#                observation_type=ObserationType.IMAGE,
+#                use_full_obs=True
+#                )
+env = RobotDelivery(n_agents=30,
+                msg_bits=0,
+                sensor_range=10,
+                request_queue_size=20,
+                max_inactivity_steps=10000,
+                max_steps=10000,
+                reward_type=RewardType.TWO_STAGE,
+                layout=layout_2way,
+                block_size="small",
+                observation_type=ObserationType.IMAGE,
+                use_full_obs=True
+                )
+
+state = env.reset()
+
+for _ in tqdm(range(1000000)):
+    env.render()
+    actions = env.action_space.sample()
+    next_obs, reward, dones, info = env.step(actions)
